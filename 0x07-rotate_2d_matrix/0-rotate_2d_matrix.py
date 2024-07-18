8bff893e166c43f6048397661c27f851b5385b97
Yasmineltair <yasmineltair293@gmail.com>
2024-07-18 20:34:14 +0300

    0

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" rotate 2d matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """main method"""
+    n = len(matrix)
+    
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            
+    for i in range(n):
+        matrix[i].reverse()
